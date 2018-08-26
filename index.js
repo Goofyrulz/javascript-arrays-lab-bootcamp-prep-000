1c5fd3c2b51c675800e1c139e69c29c31066bee1
Christopher Anderson <andersonchris227@gmail.com>
2018-08-26 18:20:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your functions and code here
+var kittens = ['milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten() {
+  kittens.push('Ralph');
+}
+function destructivelyPrependKitten() {
+  kittens.unshift('Bob');
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop(1);
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift(1)
+}
+var appendKitten =  kittens.concat(['broom']);
+
+function appendKitten() {
+  kittens.push('Broom');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function removeFirstKitten() {
+  console.log(kittens.slice(1));
+}
